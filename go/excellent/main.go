94da96eecbb4742f62207499039476e13d918ee3
pankou315 <pankou315@gmail.com>
2025-04-21 17:24:04 +0900

    ok

---
+++ go/excellent/main.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func EvenOrOdd(number int) string {
+	if number%2 == 0 {
+		return "Even"
+	}
+	return "Odd"
+}
+
+func main() {
+	fmt.Println(EvenOrOdd(10))
+	fmt.Println(EvenOrOdd(7))
+}
+
